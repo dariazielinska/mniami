2704bffcfc107f300cc229a62708595c5bf1b0fa
Daria Zielińska <44161041+dariazielinska@users.noreply.github.com>
2024-09-03 07:15:11 +0000

    MP-25 Add recipe previews display and styling

--- mniami-platform/src/pages/Recipes.js
+++ mniami-platform/src/pages/Recipes.js
@@ -2,8 +2,65 @@ import { useState, useEffect } from 'react'
 import { collection, getDocs } from 'firebase/firestore'
 import { firestore } from '../firebaseConfig'
 import { Link } from 'react-router-dom'
+import styled from '@emotion/styled'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer'
+
+const Container = styled.div`
+  width: 100%;
+`
+
+const Title = styled.h2`
+  width: 90%;
+  text-align: left;
+  font-weight: 600;
+  font-size: 18px;
+  margin: 20px auto;
+
+  @media (min-width: 768px) {
+    font-size: 22px;
+  }
+`
+
+const RecipesContainer = styled.ul`
+  width: 90%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 0;
+  margin: 0 auto;
+`
+
+const RecipePreview = styled(Link)`
+  width: 45%;
+  heigth: 100%;
+  aspect-ratio: 1;
+  text-decoration: none;
+  color: #333;
+  cursor: pointer;
+
+  @media (min-width: 768px) {
+    width: 30%;
+  }
+
+  @media (min-width: 1024px) {
+    width: 18%;
+  }
+`
+
+const Image = styled.div`
+  width: 100%;
+  heigth: 100%;
+  aspect-ratio: 1;
+  border: 1px solid #ddd;
+  background-color: #ddd;
+`
+
+const RecipeTitle = styled.h3`
+  text-align: left;
+  font-weight: 300;
+  font-size: 16px;
+`
 
 function Recipes() {
   const [recipes, setRecipes] = useState([])
@@ -32,18 +89,21 @@ function Recipes() {
   return (
     <div>
       <Header />
-      ZAKŁADKA PRZEPISY
-      {loading ? (
-        <p>Loading recipes...</p>
-      ) : (
-        <ul>
-          {recipes.map((recipe) => (
-            <Link to={`/recipe/${recipe.id}`} key={recipe.id}>
-              {recipe.title}
-            </Link>
-          ))}
-        </ul>
-      )}
+      <Container>
+        <Title>Najnowsze przepisy</Title>
+        {loading ? (
+          <p>Ładujemy przepisy...</p>
+        ) : (
+          <RecipesContainer>
+            {recipes.map((recipe) => (
+              <RecipePreview to={`/recipe/${recipe.id}`} key={recipe.id}>
+                <Image></Image>
+                <RecipeTitle>{recipe.title}</RecipeTitle>
+              </RecipePreview>
+            ))}
+          </RecipesContainer>
+        )}
+      </Container>
       <Footer />
     </div>
   )
