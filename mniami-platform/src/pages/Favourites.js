efa53ea176f2fdf831e8058fa79f79ccea04719e
Daria Zielińska <44161041+dariazielinska@users.noreply.github.com>
2024-09-11 11:57:55 +0000

    MP-38 Refactor Favourites page with grid layout

--- mniami-platform/src/pages/Favourites.js
+++ mniami-platform/src/pages/Favourites.js
@@ -1,15 +1,66 @@
 import React, { useEffect, useState } from 'react'
+import styled from '@emotion/styled'
 import { useAuth } from '../contexts/AuthProvider'
 import { firestore } from '../firebaseConfig'
 import { collection, doc, getDoc } from 'firebase/firestore'
+import { useFetchRecipes } from '../hooks/useFetchRecipes'
 import Header from '../components/Header/Header'
+import Carousel from '../components/Carousel'
 import Footer from '../components/Footer'
+import { Link } from 'react-router-dom'
+
+const Title = styled.h1`
+  width: 90%;
+  margin: 20px auto 9px auto;
+  font-size: 24px;
+  font-weight: 500;
+`
+
+const SectionTitle = styled.h2`
+  font-size: 20px;
+  margin: 15px auto;
+  width: 90%;
+  font-weight: 500;
+`
+
+const FavouritesContainer = styled.ul`
+  width: 90%;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  grid-gap: 16px;
+  padding: 0;
+  margin: 0 auto;
+`
+
+const FavouriteItem = styled(Link)`
+  text-decoration: none;
+  color: #333;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+`
+
+const Image = styled.div`
+  width: 100%;
+  height: 100%;
+  aspect-ratio: 1;
+  background-color: #ddd;
+  border: 1px solid #ddd;
+`
+
+const FavouriteTitle = styled.h3`
+  text-align: center;
+  font-weight: 300;
+  font-size: 16px;
+  padding: 10px;
+`
 
 function Favourites() {
   const { currentUser } = useAuth()
   const [favourites, setFavourites] = useState({ recipes: [], articles: [] })
   const [recipeDetails, setRecipeDetails] = useState({})
   const [articleDetails, setArticleDetails] = useState({})
+  const { latestRecipes } = useFetchRecipes()
 
   useEffect(() => {
     const fetchFavourites = async () => {
@@ -57,39 +108,51 @@ function Favourites() {
   }, [currentUser])
 
   return (
-    <div>
+    <>
       <Header />
-      ZAKŁADKA ULUBIONE
-      <h2>Favourite Recipes</h2>
+      <Title>Ulubione</Title>
+      <SectionTitle>Przepisy</SectionTitle>
       {favourites.recipes.length === 0 ? (
-        <p>No favourite recipes found.</p>
+        <p>Brak ulubionych przepisów.</p>
       ) : (
-        <ul>
+        <FavouritesContainer>
           {favourites.recipes.map((recipeId) => (
-            <li key={recipeId}>
-              {recipeDetails[recipeId]
-                ? recipeDetails[recipeId].title
-                : 'Loading...'}
-            </li>
+            <FavouriteItem key={recipeId} to={`/recipe/${recipeId}`}>
+              <Image />
+              <FavouriteTitle>
+                {recipeDetails[recipeId]
+                  ? recipeDetails[recipeId].title
+                  : 'Ładowanie...'}
+              </FavouriteTitle>
+            </FavouriteItem>
           ))}
-        </ul>
+        </FavouritesContainer>
       )}
-      <h2>Favourite Articles</h2>
+      <SectionTitle>Artykuły</SectionTitle>
       {favourites.articles.length === 0 ? (
-        <p>No favourite articles found.</p>
+        <p>Brak ulubionych artykułów.</p>
       ) : (
-        <ul>
+        <FavouritesContainer>
           {favourites.articles.map((articleId) => (
-            <li key={articleId}>
-              {articleDetails[articleId]
-                ? articleDetails[articleId].title
-                : 'Loading...'}
-            </li>
+            <FavouriteItem key={articleId} to={`/article/${articleId}`}>
+              <Image />
+              <FavouriteTitle>
+                {articleDetails[articleId]
+                  ? articleDetails[articleId].title
+                  : 'Ładowanie...'}
+              </FavouriteTitle>
+            </FavouriteItem>
           ))}
-        </ul>
+        </FavouritesContainer>
       )}
+      <Carousel
+        type={'Przeglądaj nasze najnowsze przepisy'}
+        items={latestRecipes}
+        category="recipe"
+        viewAllLink="/recipes"
+      />
       <Footer />
-    </div>
+    </>
   )
 }
 
