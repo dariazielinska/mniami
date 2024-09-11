f5e379ec77017fdb46f8651e1554e389a6fa0223
Daria Zielińska <44161041+dariazielinska@users.noreply.github.com>
2024-09-11 08:52:42 +0000

    MP-35 Add AddToShoppingList button with DB request

--- mniami-platform/src/pages/Recipe/Ingredients.js
+++ mniami-platform/src/pages/Recipe/Ingredients.js
@@ -1,23 +1,80 @@
 import styled from '@emotion/styled'
+import { doc, updateDoc, arrayUnion } from 'firebase/firestore'
+import { firestore } from '../../firebaseConfig'
+import { useAuth } from '../../contexts/AuthProvider'
 
-const IngredientsContainer = styled.div``
+const IngredientsContainer = styled.div`
+  width: 90%;
+  margin: 0 auto;
+`
 
 const IngredientsTitle = styled.h2`
-  width: 90%;
-  margin: 20px auto 9px auto;
+  margin: 20px 0 9px 0;
   font-size: 22px;
   font-weight: 500;
 `
 
 const IngredientsList = styled.ul`
-  width: 90%;
-  margin: 0 auto;
   font-size: 20px;
+  padding-inline-start: 20px;
 `
 
 const IngredientItem = styled.li``
 
+const AddToCartButton = styled.button`
+  padding: 7px;
+  font-size: 16px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  border: 1px solid transparent;
+  border-radius: 10px;
+  text-decoration: none;
+  color: #333;
+  text-align: center;
+  cursor: pointer;
+  margin: 15px 0;
+  width: 100%;
+  display: block;
+
+  @media (min-width: 768px) {
+    width: 45%;
+  }
+
+  @media (min-width: 1024px) {
+    width: 30%;
+  }
+`
+
 const Ingredients = ({ ingredients }) => {
+  const { currentUser } = useAuth()
+
+  const addToShoppingList = async () => {
+    if (!currentUser) {
+      console.log('User is not logged in')
+      return
+    }
+
+    const shoppingListRef = doc(firestore, 'shoppingLists', currentUser.uid)
+
+    const formattedIngredients = ingredients.map((ingredient) => ({
+      name: ingredient.name,
+      quantity: ingredient.quantity,
+      unit: ingredient.unit,
+    }))
+
+    try {
+      await updateDoc(shoppingListRef, {
+        items: arrayUnion(...formattedIngredients),
+      })
+      console.log('Ingredients added to shopping list')
+    } catch (error) {
+      console.error('Error adding ingredients to shopping list: ', error)
+    }
+  }
+
+  const handleAddToShoppingList = () => {
+    addToShoppingList()
+  }
+
   if (!ingredients || ingredients.length === 0) {
     return <p>Wystąpił błąd</p>
   }
@@ -32,6 +89,9 @@ const Ingredients = ({ ingredients }) => {
           </IngredientItem>
         ))}
       </IngredientsList>
+      <AddToCartButton onClick={handleAddToShoppingList}>
+        Dodaj do listy zakupów
+      </AddToCartButton>
     </IngredientsContainer>
   )
 }
