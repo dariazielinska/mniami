cb1d1dd2c3df69e7cbfd06d1064a5717fb60b1b7
Daria Zielińska <44161041+dariazielinska@users.noreply.github.com>
2024-09-19 10:39:28 +0000

    MP-44 Fix wrong import name

--- mniami-platform/src/pages/Article.js
+++ mniami-platform/src/pages/Article.js
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer'
-import { useFetchArticle } from '../hooks/useArticle'
+import { useFetchArticle } from '../hooks/useFetchArticle'
 
 const Article = () => {
   const { id } = useParams()
